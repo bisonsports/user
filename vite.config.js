21a9cf295f17dbe50be2d08bf5abe920d450fdcd
bisonsports <bisonsports@gmail.com>
2025-03-02 16:48:21 +0530

    jsx issue

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   base: "/user/",
   plugins: [react()],
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx']
+  },
+  optimizeDeps: {
+    include: ['react', 'react-dom']
+  },
+  esbuild: {
+    loader: 'jsx',
+    include: /src\/.*\.jsx?$/,
+    exclude: []
+  }
 });
