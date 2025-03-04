ec7f19f090e9b5216f39c65b9dac6eb6aa43232b
bisonsports <bisonsports@gmail.com>
2025-03-04 14:11:00 +0530

    build push

--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: "/user/",
   plugins: [react()],
-  resolve: {
-    extensions: ['.js', '.jsx', '.ts', '.tsx']
-  },
-  optimizeDeps: {
-    include: ['react', 'react-dom']
-  },
-  esbuild: {
-    loader: 'jsx',
-    include: /src\/.*\.jsx?$/,
-    exclude: []
-  }
+  base: '/user', // Change "user" to your actual repo name
 });
